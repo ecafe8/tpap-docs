e5f67dac9b135c29100d025f9a4c9803bcbdab37
ecafe8 <windzb@qq.com>
2013-09-12 19:58:34 +0800

    Signed-off-by: ecafe8 <windzb@qq.com>

---
+++ source/raw/tpap/kissy/1.3.0/component/resizable/resizable.php
@@ -0,0 +1,12 @@
+<!--include kissy core and resizable file-->
+<cajamodules include="kissy/1.3.0/core,kissy/1.3.0/resizable"/>
+
+<!--refer js file-->
+<script src="/kissy/1.3.0/component/calendar/resizable.js"></script>
+
+<h1>DEMO</h1>
+
+<div class="resizable_box">
+  	<div class="content"><img src="http://img02.taobaocdn.com/tfscom/i3/T1OLJwFh0fXXb1upjX_310x310.jpg" alt=""></div>
+  	<span>取消</span>
+</div>
